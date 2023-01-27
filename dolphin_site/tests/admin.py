b16d9a08ef4533888512d28dfc2045457b93ff39
JC <jonathancavia0101@gmail.com>
2023-01-27 09:43:35 -0300

    validators range of atributos

--- dolphin_site/tests/admin.py
+++ dolphin_site/tests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Delfin,Atributo,Atributo_Delfin
+
+admin.site.register(Delfin)
+admin.site.register(Atributo)
+admin.site.register(Atributo_Delfin)
--- dolphin_site/tests/models.py
+++ dolphin_site/tests/models.py
@@ -1,19 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Delfin(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('date published')
+
+
+    def __str__(self):
+        return "{nombre}: {descripcion}".format(nombre = self.nombre,descripcion=self.descripcion)
 
 
 class Atributo(models.Model):
     """para no hardcodear los atributos"""
     descripcion = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.descripcion
+    
+
 class Atributo_Delfin(models.Model):
     """un delfin tiene muchos atributos"""
     delfin = models.ForeignKey(Delfin, on_delete=models.CASCADE)
     atributo = models.ForeignKey(Atributo, on_delete=models.CASCADE)
-    valor = models.IntegerField(default=0)
+    valor = models.IntegerField(default=0,validators=[MinValueValidator(-2),MaxValueValidator(2)])
